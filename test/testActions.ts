382fa92c515d98dbba3b481c03e09503312588d5
Vince Au <vinceau09@gmail.com>
2019-12-31 08:49:31 +1100

    Reduce delay

--- test/testActions.ts
+++ test/testActions.ts
@@ -5,28 +5,28 @@ import { delay }  from "../src/utils";
 
 export class AddOne implements Action {
   public async run(...args: any[]): Promise<any> {
-    await delay(1000);
+    await delay(200);
     return args[0] + 1;
   }
 }
 
 export class SubOne implements Action {
   public async run(...args: any[]): Promise<any> {
-    await delay(1000);
+    await delay(300);
     return args[0] - 1;
   }
 }
 
 export class AddTwo implements Action {
   public async run(...args: any[]): Promise<any> {
-    await delay(1000);
+    await delay(200);
     return args[0] + 2;
   }
 }
 
 export class AddFive implements Action {
   public async run(...args: any[]): Promise<any> {
-    await delay(1000);
+    await delay(200);
     return args[0] + 5;
   }
 }
